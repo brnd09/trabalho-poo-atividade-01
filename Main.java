b00bffa68b9f056a3c84c75d5ec2c38125ceb89e
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-04-29 19:49:12 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
-package org.example;
+      import javax.swing.*;
+      import java.awt.*;
 
-public class Main {
-    public static void main(String[] args) {
+      public class Main {
+
+        // Define o plano de fundo para os diálogos do JOptionPane
+          public static void setCustomBackgroundColor(Color color) {
+              UIManager.put("OptionPane.background", color);
+              UIManager.put("Panel.background", color);
+          }
+
+        public static void main(String[] args) {
+
+          Color verde = new Color(10, 105, 10);
+          setCustomBackgroundColor(verde);
         Municipio municipio = new Municipio();
 
         // Número de imóveis a serem cadastrados
